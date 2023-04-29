12f9d2c4312f685d47bd224ff8bddfca8025ce6c
Austin Patton <SCS374Group1@gmail.com>
2023-04-29 14:33:30 -0400

    Initial Commit

---
+++ SCS 141 Final Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCS 141 Final Project
+//
+//  Created by Austin Patton on 4/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
